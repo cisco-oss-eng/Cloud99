a771868c619b5fc27409aeba01a3ccea4c649f95
bdastur <bdastur@cisco.com>
2015-05-05 12:38:47 -0700

    Ansible monitoring - process check

--- monitors/plugins/ansiblemonitor.py
+++ monitors/plugins/ansiblemonitor.py
@@ -4,10 +4,18 @@
 import sys
 import os
 import time
+import datetime
 import yaml
+import collections
 import ansible.runner
 from monitors.baseMonitor import BaseMonitor
 import ha_engine.ha_infra as infra
+
+SERVICE_LIST = [
+    {'service': 'neutron_server', 'role': 'controller'},
+    {'service': 'glance-api', 'role': 'controller'},
+    {'service': 'glance-registry', 'role': 'controller'},
+    {'service': 'foobar', 'role': 'controller'}]
 
 
 def get_absolute_path_for_file(path, file_name, splitdir=None):
@@ -82,19 +90,18 @@ class ConfigHelper(object):
 
         host_list = self.parsed_data.keys()
 
-        #for host in self.parsed_data.keys():
-        #    host_list.append(host)
-
         return host_list
 
-    def get_host_ip_list(self):
+    def get_host_ip_list(self, role=None):
         '''
         Get the list of host ip addresses
         '''
         host_ip_list = []
         for host in self.parsed_data.keys():
+            hrole = self.parsed_data[host].get('role', None)
+            if role is not None and role != hrole:
+                continue
             ip = self.parsed_data[host].get('ip', None)
-            #ip = host.get('ip', None)
             host_ip_list.append(ip)
 
         return host_ip_list
@@ -226,40 +233,129 @@ class AnsibleRunner(object):
 
         return results
 
+    def ansible_execute_command(self, host_list=None, remote_user=None,
+                                args=None, complex_args=None, forks=2):
+        '''
+        Execute a command on remote host using Ansible 'command'
+        module
+        '''
+
+        (host_list, remote_user) = \
+            self.validate_host_parameters(host_list, remote_user)
+        if (host_list, remote_user) is (None, None):
+            return None
+
+        if args is None or len(args) == 0:
+            print "Invalid command [%s]" % args
+            return None
+
+        cmd = " ".join(args)
+
+        runner = ansible.runner.Runner(
+            module_name="command",
+            host_list=host_list,
+            remote_user=remote_user,
+            module_args=cmd,
+            complex_args=complex_args,
+            forks=forks)
+
+        results = runner.run()
+
+        results = self.validate_results(results, checks=[{'stderr': ''}])
+        if results['status'] != 'PASS':
+            print "Execute command %s failed" % cmd
+
+        return results
+
 
 class AnsibleMonitor(BaseMonitor):
     '''
     Anisble Monitor.
     '''
+    def get_monitor_timestamp(self):
+        '''
+        Return the timestamp that will be added to the
+        results.
+        '''
+        dt = datetime.datetime.now()
+        timestamp = "%s:%s:%s-%s/%s/%s" % (dt.second, dt.minute, dt.hour,
+                                           dt.month, dt.day, dt.year)
+
+        return timestamp
+
+    def ansible_check_process(self,
+                              host_list, remote_user, process_name):
+        '''
+        Check the process status
+        '''
+        ansi_result = {}
+        ansi_result['name'] = "process_check"
+        ansi_result['process'] = process_name
+
+        args = "ps -ef | grep %s | grep -v grep" % process_name
+        ansi_result['ansi_result'] = self.ansirunner.\
+            ansible_perform_operation(host_list=host_list,
+                                      remote_user=remote_user,
+                                      module="shell",
+                                      module_args=args)
+
+        #print "RESULTS:::: ", ansi_result['ansi_result']
+        host0 = ansi_result['ansi_result']['contacted'][host_list[0]]
+
+        infra.display_on_terminal(self, host0['stdout'])
+
+        return ansi_result
+
     def start(self, sync=None, finish_execution=None, args=None):
         '''
         Required start method to implement for the class.
         '''
+        # Initialize and Parse user input..
+        self.ansirunner = None
+        self.ansiresults = collections.deque(maxlen=5)
         setup_file = "../../configs/openstack_config.yaml"
-
         inventory = ConfigHelper(host_file=setup_file)
+
         print "parsed data: ", inventory.parsed_data
         print "host list: ", inventory.get_host_list()
 
         data = self.get_input_arguments()
-        print "data: ", data
+        print "User data: ", data
+        print "freq: ", data[0]['ansible']['frequency']
+        self.frequency = data[0]['ansible'].get('frequency', 5)
+
+
         host_list = inventory.get_host_list()
         host_ip_list = inventory.get_host_ip_list()
+        control_ip_list = inventory.get_host_ip_list(role='controller')
+        compute_ip_list = inventory.get_host_ip_list(role='compute')
         remote_user = inventory.get_host_username(host_list[0])
-
-        print "host ip list: ", host_ip_list
+        print "[all: %s], [control: %s] [compute: %s]" % \
+            (host_ip_list, control_ip_list, compute_ip_list)
         print "Remote user: ", remote_user
+        self.ansirunner = AnsibleRunner()
 
         while True:
-            ansirunner = AnsibleRunner()
-            ansirunner.ansible_perform_operation(host_list=host_ip_list,
-                                                 remote_user=remote_user,
-                                                 module="ping")
+            ####################################################
+            # Ansible Monitoring Loop.
+            ####################################################
+            ts_results = []
+            ts = self.get_monitor_timestamp()
+            ts_results.append({'ts': ts})
 
+            for service in SERVICE_LIST:
+                host_ip_list = inventory.get_host_ip_list(role=service['role'])
+                ansi_results = self.ansible_check_process(host_ip_list,
+                                                          remote_user,
+                                                          service['service'])
+            ts_results.append(ansi_results)
             infra.display_on_terminal(self,
                                       "Ansible Module: test")
-            time.sleep(5)
 
+            # Add the ts results to main result list.
+            self.ansiresults.append(ts_results)
+
+            time.sleep(self.frequency)
 
 
 
