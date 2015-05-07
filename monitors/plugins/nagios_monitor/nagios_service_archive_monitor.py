65cefc4e2ca0c9a9b3ea70a20f3fb669228f9512
ubuntu <ubuntu@outbound.cisco.com>
2015-05-07 15:16:08 -0700

    nagios changes

--- monitors/plugins/nagios_monitor/nagios_service_archive_monitor.py
+++ monitors/plugins/nagios_monitor/nagios_service_archive_monitor.py
@@ -27,6 +27,7 @@ URL = "http://%s/nagios/cgi-bin/archivejson.cgi?query=availability&availabilityo
 
 class service_archive_data(object):
 
+	@staticmethod
 	def getdata(url,ipaddress,starttime,endtime):
 		try:
 			# request = urllib2.Request("http://172.22.191.244/nagios/cgi-bin/archivejson.cgi?query=availability&availabilityobjecttype=services&starttime=1429221978&endtime=1429826778")
@@ -40,6 +41,7 @@ class service_archive_data(object):
 		data = json.loads(json_data)
 		return data["data"]#["services"]
 
+	@staticmethod
 	def get_severity_color(severity,text):
 		# print severity, text
 		if severity == 'CRITICAL':
@@ -49,12 +51,12 @@ class service_archive_data(object):
 		elif severity == 'INFO':
 			return "\033[" + '32' + "m" + text + "\033[0m"
 
-
-
+	@staticmethod
 	def do_calc(starttime,endtime,value):
 		delta = float((endtime - starttime)/1000)
 		return round((float(value)/delta)*100,2)
 
+	@staticmethod
 	def filter_objs(objs):
 		filter_objs = []
 		for obj in objs:
@@ -62,72 +64,69 @@ class service_archive_data(object):
 				filter_objs.append(obj)
 		return filter_objs
 
+	@staticmethod
 	def get_config(key,section='Default'):
 		config = ConfigParser.ConfigParser()
 		path = os.getcwd()+os.sep+'nagios_config.cfg'
 		config.readfp(open(path))
 		return config.get(section,key)
 
+	@staticmethod
+	def generateData(starttime):
+		format_string = "%s \t %s \t\t\t %s \t %s \t %s "
+		print get_severity_color('INFO',format_string%('IP address',"Description".ljust(40),"Time ok","Time critical","Time indeterminate nodata"))
 
-# =================== main ================
-if __name__ == '__main__':
-	format_string = "%s \t %s \t\t\t %s \t %s \t %s "
-	print get_severity_color('INFO',format_string%('IP address',"Description".ljust(40),"Time ok","Time critical","Time indeterminate nodata"))
+		i=0   
+		args = []
+		starttime = ''
+		endtime = 0
+		inputs = sys.argv[1:]
+		for arg in inputs:
+			if arg  == 'ip':
+				IP_FILTER  = inputs[i+1].split(',')
+			elif arg == 'int':
+				args = inputs[i+1].split(',')
+			elif arg == 'fre':
+				POLLING_INTERVAL = float(inputs[i+1])
+			i=i+1
+		if args != []:
+			starttime = args[0]
+			endtime = args[1]
+		else:
+			starttime = get_config('start_time')
+			endtime = get_config('end_time')
+		#print "Start time : %s, End Time : %s , Polling Interval %s " % (starttime,endtime,POLLING_INTERVAL)
+		#print "IP Filter : %s " % IP_FILTER
+		
+		
+		ip_address = get_config('nagios_ip')
 
-	i=0   
-	args = []
-	starttime = ''
-	endtime = ''
-	inputs = sys.argv[1:]
-	for arg in inputs:
-		if arg  == 'ip':
-			IP_FILTER  = inputs[i+1].split(',')
-		elif arg == 'int':
-			args = inputs[i+1].split(',')
-		elif arg == 'fre':
-			POLLING_INTERVAL = float(inputs[i+1])
-		i=i+1
-	if args != []:
-		starttime = args[0]
-		endtime = args[1]
-	else:
-		starttime = get_config('start_time')
-		endtime = get_config('end_time')
-	print "Start time : %s, End Time : %s , Polling Interval %s " % (starttime,endtime,POLLING_INTERVAL)
-	print "IP Filter : %s " % IP_FILTER
-	ip_address = get_config('nagios_ip')
-
-	# Execution starts here
-	entries=0
-	while(True):
+		# Execution starts here
+		entries=0
 		data = getdata(URL,ip_address,starttime,endtime)
 		# Filtering based on ip
-		if IP_FILTER == []:
-			services = data['services']
-		else:
-			services = filter_objs(data['services'])
-			# print services
+		services = data['services']
 		selectors = data['selectors']
 		ret = []
 		for obj in services:
 			result = {}
 			for header in HEADERS:
-				if header in ['time_critical','time_ok','time_indeterminate_nodata']:
-					percentage = do_calc(selectors['starttime'],selectors['endtime'],obj[header])
-					# setting severity to critical time
-					if header == 'time_critical' and percentage > 0:
-						result[header] = get_severity_color('CRITICAL',str(obj[header]) +'('+str(percentage)+'%)')
+					if header in ['time_critical','time_ok','time_indeterminate_nodata']:
+						percentage = do_calc(selectors['starttime'],selectors['endtime'],obj[header])
+						# setting severity to critical time
+						if header == 'time_critical' and percentage > 0:
+							result[header] = get_severity_color('CRITICAL',str(obj[header]) +'('+str(percentage)+'%)')
+						else:
+							result[header] = str(obj[header]) +'('+str(percentage)+'%)'
 					else:
-						result[header] = str(obj[header]) +'('+str(percentage)+'%)'
-				else:
-					result[header] = obj[header]
-			ret.append(result)
-		for item in ret:
-			# tuncating the description to 30 char for better visibilty ljust()
-			print format_string%(item.get(HEADERS[0]),item.get(HEADERS[1])[:40].ljust(40),item.get(HEADERS[3]),item.get(HEADERS[2]),item.get(HEADERS[4]))
-			entries=entries+1
-			if entries >= 20:
-				print get_severity_color('INFO',format_string%('IP address',"Description".ljust(40),"Time ok","Time critical","Time indeterminate nodata"))
-				entries=0
+						result[header] = obj[header]
+				ret.append(result)
+			for item in ret:
+				# tuncating the description to 30 char for better visibilty ljust()
+				print format_string%(item.get(HEADERS[0]),item.get(HEADERS[1])[:40].ljust(40),item.get(HEADERS[3]),item.get(HEADERS[2]),item.get(HEADERS[4]))
+				entries=entries+1
+				if entries >= 20:
+					print get_severity_color('INFO',format_string%('IP address',"Description".ljust(40),"Time ok","Time critical","Time indeterminate nodata"))
+					entries=0
 
-		time.sleep(POLLING_INTERVAL)
+			time.sleep(POLLING_INTERVAL)
